945d2db19a0fd73e4dcba97b610eeff0926193c3
estherevellyn <estherevellyn@gmail.com>
2023-09-01 11:43:30 -0300

    estrutura de captura js

---
+++ html/atividade 13/script.js
@@ -0,0 +1,24 @@
+
+const titulo = document.getElementsByTagName('h1')
+
+const paragrafo = document.getElementsByTagName('p')
+
+const quadrado = document.getElementsByClassName('quadrado')
+
+const div2 = document.getElementsByClassName('circulo')
+
+
+//querySector
+//querySelectorAll
+
+const lista = document.querySelectorAll('p')
+
+
+console.log(titulo)
+
+//console.log(paragrafo)
+
+//console.log(quadrado)
+
+//console.log(div2)
+//console.log(lista)
