355c8caaf8b7263f6312d20626ae514ab34833e3
estherevelyn <estherevellyn@gmail.com>
2023-09-06 11:46:21 -0300

    JavaScript erro do email

--- html/atividade 15/script.js
+++ html/atividade 15/script.js
@@ -1,10 +1,19 @@
 const email = document.getElementById('email')
 const senha = document.getElementById('senha')
-const form = document.getElementById('formulario')
 const entrar = document.getElementById('entrar')
 
-entrar.addEventListener('click', () => {
+entrar.addEventListener('click', (event) => {
     
-    envia
-    console.log('vim de uma arrow function')
-}
+    event.preventDefault();
+
+    if (email.value == ''){
+        alert('Email invalido') 
+        email.classList = 'erro'
+       
+    }
+    if (senha.value == ''){
+        alert('Senha invalido!')
+        senha.classList = 'erro'
+    }
+
+})
