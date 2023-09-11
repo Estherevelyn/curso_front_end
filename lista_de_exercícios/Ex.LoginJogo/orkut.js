b838dd5304e57f7856cfeced0ec44e3783863535
Estherevellyn <estherevellyn@gmail.com>
2023-09-11 00:50:27 -0300

    atividade orkut

---
+++ lista_de_exercícios/Ex.LoginJogo/orkut.js
@@ -0,0 +1,12 @@
+const email = document.getElementById(`iemail`)
+const senha = document.getElementById(`isenha`)
+const login = document.getElementById(`ilogin`)
+
+login.addEventListener(`click`, (erro)) => {
+
+
+}
+
+
+
+
