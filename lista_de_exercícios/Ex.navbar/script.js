cdd59eaaf23e817fcf69680247fa95e54b1d3d94
Estherevellyn <estherevellyn@gmail.com>
2023-09-12 00:42:39 -0300

    navbar

---
+++ lista_de_exercícios/Ex.navbar/script.js
@@ -0,0 +1,8 @@
+const humburguer = document.querySelector(`.humburguer`);
+const navMenu = document.querySelector(`.nav-menu`);
+
+
+humburguer.addEventListener(`click`, () => {
+    humburguer.classList.toggle(`active`);
+    humburguer.classList.toggle(`active`);
+})
